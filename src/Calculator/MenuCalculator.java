0cff30f178e1936c4924e89cd899f4652e477225
Hejociara <hejociara@gmail.com>
2022-01-24 13:48:39 -0300

    feature: new classes Calculator and MenuCalculator

---
+++ src/Calculator/MenuCalculator.java
@@ -0,0 +1,18 @@
+package Calculator;
+
+/**
+ * @author : Hejociara de Nazaré do Vale
+ * 24/01/2022
+ *
+ */
+public class MenuCalculator {
+    public static void MenuCalculator(){
+        System.out.println("         Welcome to calculator       " );
+        System.out.println("Please select one option calculator you want");
+        System.out.println("1 - Standard calculator");
+        System.out.println("2 - IMC calculator" );
+        System.out.println("3 - Calculator area" );
+        System.out.println("4- Exit" );
+
+    }
+}
