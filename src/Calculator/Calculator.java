13c3f8f554db1796c9fa5f53522c480303b9ff58
Hejociara <hejociara@gmail.com>
2022-08-15 21:11:48 -0300

    Features

--- src/Calculator/Calculator.java
+++ src/Calculator/Calculator.java
@@ -1,5 +1,37 @@
 package Calculator;
 
-public class Calculator {
+import java.util.Scanner;
 
+public class Calculator {
+    public static void Calculator(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int x,y;
+
+        System.out.println("Type the first value: ");
+        x = scan.nextInt();
+        System.out.println("Type the second value: ");
+        y = scan.nextInt();
+
+        int addition = addition (x,y);
+        System.out.println("addition: " + addition);
+        int subtraction = subtraction (x,y);
+        System.out.println("sub: " + subtraction);
+         int multiplication = multiplication (x,y);
+        System.out.println("mult: " + multiplication);
+         int division = division (x,y);
+        System.out.println("division: " + subtraction);
+
+    }
+    public static int addition (int x, int y){
+        return x + y;
+    }
+    public static int subtraction (int x, int y){
+        return x - y;
+    }
+    public static int multiplication (int x, int y){
+        return x * y;
+    }
+    public static int division (int x, int y){
+        return x / y;
+    }
 }
---
+++ src/Calculator/CalculatorMenu.java
@@ -0,0 +1,47 @@
+package Calculator;
+
+import java.util.Scanner;
+
+/**
+ * @author : Hejociara de Nazaré do Vale
+ * Date: 24/01/2022
+ *Since: 1.0
+ */
+public class CalculatorMenu {
+    public static void main(String[] args) {
+
+        Scanner menu = new Scanner(System.in);
+
+            System.out.println("         Welcome to calculator       ");
+            System.out.println("Please, insert an option: ");
+            System.out.println("1 - Standard calculator");
+            System.out.println("2 - IMC calculator");
+            System.out.println("3 - Area calculator ");
+            System.out.println("4 - Exit");
+
+            int option = menu.nextInt();
+
+            switch (option) {
+                case 1:
+                    System.out.print("\nStandard calculator");
+                    break;
+
+                case 2:
+                    System.out.print("\nIMC calculator\n");
+                    break;
+
+                case 3:
+                    System.out.print("\nArea calculator\n");
+                    break;
+
+                default:
+                    System.out.print("\nInvalid Option!");
+                    break;
+
+                case 4:
+                    System.out.print("\nSee you soon!");
+                    menu.close();
+            }
+        }
+    }
+
